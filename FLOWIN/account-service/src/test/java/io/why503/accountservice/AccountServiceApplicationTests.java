9e11929c727f929efff9e30461d9ce5a4b2ac9ca
hansj4873 <hansengjuu@naver.com>
2026-01-06 11:43:30 +0900

    [FI-00] 프로젝트 생성
    +gateway
    +discovery
    +account
    +reservation
    +company

---
+++ FLOWIN/account-service/src/test/java/io/why503/accountservice/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.why503.accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

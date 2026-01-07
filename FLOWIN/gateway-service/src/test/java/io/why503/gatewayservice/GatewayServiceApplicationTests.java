4cfc1b806cd763847c093f119d36accbc3d80abe
kdhaw <101175633+kdhaw@users.noreply.github.com>
2026-01-07 11:26:27 +0900

    Add files via upload

---
+++ FLOWIN/gateway-service/src/test/java/io/why503/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.why503.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

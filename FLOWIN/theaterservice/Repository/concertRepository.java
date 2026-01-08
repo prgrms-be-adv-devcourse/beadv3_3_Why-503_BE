f0fa1613c99d232f427bdb6aedf81a52014fc3b5
kdhaw <101175633+kdhaw@users.noreply.github.com>
2026-01-08 16:55:11 +0900

    Delete FLOWIN/theaterservice/Repository directory

--- FLOWIN/theaterservice/Repository/concertRepository.java
+++
@@ -1,9 +0,0 @@
-package io.why503.theaterservice.Repository;
-
-import io.why503.theaterservice.model.Entity.concert_Item;
-import io.why503.theaterservice.model.dto.hallResponse;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface concertRepository extends JpaRepository<concert_Item, Long> {
-    hallResponse findByStat(concert_Item concert_Item);
-}
--- FLOWIN/theaterservice/Repository/hallRepository.java
+++
@@ -1,9 +0,0 @@
-package io.why503.theaterservice.Repository;
-
-import io.why503.theaterservice.model.Entity.concert_hall;
-import io.why503.theaterservice.model.dto.hallResponse;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface hallRepository extends JpaRepository<concert_hall, Long> {
-    hallResponse findById(concert_hall concert);
-}

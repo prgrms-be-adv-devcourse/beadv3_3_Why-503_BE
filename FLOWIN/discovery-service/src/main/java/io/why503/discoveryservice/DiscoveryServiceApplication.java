ba6f9ee39ca33018c040421946cc41f3f1babfd3
hansj4873 <hansengjuu@naver.com>
2026-01-12 15:32:33 +0900

    [FI-201] discovery 기본
    기본적인 eureka서버입니다.

--- FLOWIN/discovery-service/src/main/java/io/why503/discoveryservice/DiscoveryServiceApplication.java
+++ FLOWIN/discovery-service/src/main/java/io/why503/discoveryservice/DiscoveryServiceApplication.java
@@ -2,7 +2,9 @@ package io.why503.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer // discovery 필수 어노테이션
 @SpringBootApplication
 public class DiscoveryServiceApplication {
 

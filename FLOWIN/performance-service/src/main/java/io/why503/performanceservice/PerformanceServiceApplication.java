24808e6f9756a018b02d0e56665ad17c905c1adf
S2Ruby <jwzang1004@gmail.com>
2026-01-13 13:58:46 +0900

    feat: 퍼포먼스 서비스 추가

---
+++ FLOWIN/performance-service/src/main/java/io/why503/performanceservice/PerformanceServiceApplication.java
@@ -0,0 +1,13 @@
+package io.why503.performanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceServiceApplication.class, args);
+	}
+
+}

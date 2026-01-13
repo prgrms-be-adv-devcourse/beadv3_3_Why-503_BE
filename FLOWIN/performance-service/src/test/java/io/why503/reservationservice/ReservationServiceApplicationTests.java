931b3c7ff66c1a6fd09aefa2de66b698bdb4f6d7
kdhaw <0517dhkim@gmail.com>
2026-01-13 12:23:01 +0900

    feat: 퍼포먼스 서비스 추가

---
+++ FLOWIN/performance-service/src/test/java/io/why503/reservationservice/ReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.why503.reservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

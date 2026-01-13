21935ae531a5b2ae4dd9b52a9ce301046dfdc7d0
kimgiwoong127 <106870449+kimgiwoong127@users.noreply.github.com>
2026-01-13 10:39:52 +0900

    Update AuthCodeGenerator.java

--- FLOWIN/company-service/src/main/java/io/why503/companyservice/utill/AuthCodeGenerator.java
+++ FLOWIN/company-service/src/main/java/io/why503/companyservice/utill/AuthCodeGenerator.java
@@ -4,16 +4,16 @@
  * 사용 목적 :
  * - 이메일 인증에 사용할 일회성 인증 코드 생성
  */
-package io.why503.companyservice.utill;
+package io.why503.companyservice.util;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 public class AuthCodeGenerator {
 
-    public static String generate() {
+    private static final SecureRandom random = new SecureRandom();
 
-        Random random = new Random(); // 난수 생성을 위한 Random 객체
-        int code = 100000 + random.nextInt(900000); // 6자리 숫자 인증 코드 생성
-        return String.valueOf(code); // 문자열 형태로 변환하여 반환
+    public static String generate() {
+        int code = 100000 + random.nextInt(900000);
+        return String.valueOf(code);
     }
 }

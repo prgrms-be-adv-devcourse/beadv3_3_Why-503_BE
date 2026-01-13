17fc1d6e6d3865714bf0e886a5c2382fcc435d9d
KKO <jwjang1102@smail.kongju.ac.kr>
2026-01-13 09:12:04 +0900

    Delete FLOWIN/theaterservice directory
    remove: 잘못 만들어진 패키지

--- FLOWIN/theaterservice/Repo/HallRepo.java
+++
@@ -1,9 +0,0 @@
-package io.why503.theaterservice.Repo;
-
-import io.why503.theaterservice.model.Ett.concert_hallEtt;
-import io.why503.theaterservice.model.Dto.hallRes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface hallRepo extends JpaRepository<concert_hallEtt, Long> {
-    hallRes findById(concert_hallEtt concert);
-}
--- FLOWIN/theaterservice/model/Dto/HallRes.java
+++
@@ -1,8 +0,0 @@
-package io.why503.theaterservice.model.Dto;
-
-public record hallRes(
-        int stat,
-        int seat_scale
-) {
-
-}

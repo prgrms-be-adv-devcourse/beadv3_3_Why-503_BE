87d90c6175a817f32e4b72dbeb7cb64238b6db84
S2Ruby <jwzang1004@gmail.com>
2026-01-15 16:11:32 +0900

    [FI-55] 주석 수정

--- FLOWIN/payment-service/src/main/java/io/why503/paymentservice/domain/booking/repo/BookingRepo.java
+++ FLOWIN/payment-service/src/main/java/io/why503/paymentservice/domain/booking/repo/BookingRepo.java
@@ -19,5 +19,10 @@ public interface BookingRepo extends JpaRepository<Booking, Long> {
     @EntityGraph(attributePaths = {"tickets"})
     Optional<Booking> findByBookingSq(Long bookingSq);
 
+    /**
+     * 만료된 예매 조회 (스케줄러용)
+     * - BookingStat이 일치하고(And), BookingDt가 입력 시간보다 이전(Before)인 데이터 조회
+     * - 즉, "특정 상태(PENDING)이면서 + 생성된 지 오래된" 예매를 찾습니다.
+     */
     List<Booking> findExpired(BookingStat status, LocalDateTime dateTime);
 }

ce7d5e75c8eefcc9c7139268e1607e194122f478
S2Ruby <jwzang1004@gmail.com>
2026-01-18 02:00:24 +0900

    [FI-02] CI 테스트를 위해 파일 수정

--- FLOWIN/payment-service/src/test/java/io/why503/paymentservice/PaymentServiceApplicationTests.java
+++ FLOWIN/payment-service/src/test/java/io/why503/paymentservice/PaymentServiceApplicationTests.java
@@ -2,6 +2,7 @@ package io.why503.paymentservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")

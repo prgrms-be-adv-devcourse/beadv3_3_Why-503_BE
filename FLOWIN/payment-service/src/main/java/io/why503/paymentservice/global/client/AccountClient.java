8e6907b940d0690c5024bc3421a8e8e44f29db27
S2Ruby <jwzang1004@gmail.com>
2026-01-19 11:04:53 +0900

    [FI-50] API 주소 수정

--- FLOWIN/payment-service/src/main/java/io/why503/paymentservice/global/client/AccountClient.java
+++ FLOWIN/payment-service/src/main/java/io/why503/paymentservice/global/client/AccountClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "account-service")
 public interface AccountClient {
     // 회원 상세 조회 API 주소
-    @GetMapping("/account/{sq}")
+    @GetMapping("/account/sq/{sq}")
     AccountResponse getAccount(@PathVariable("sq") Long userSq);
 }
 

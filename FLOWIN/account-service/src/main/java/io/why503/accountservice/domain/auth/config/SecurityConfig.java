3d81bc2e06c2085933c56a7ef691e23521efd6a8
hansj4873 <hansengjuu@naver.com>
2026-01-20 15:23:53 +0900

    [FI-208] 로그아웃 구현 완료

--- FLOWIN/account-service/src/main/java/io/why503/accountservice/domain/auth/config/SecurityConfig.java
+++ FLOWIN/account-service/src/main/java/io/why503/accountservice/domain/auth/config/SecurityConfig.java
@@ -1,6 +1,10 @@
 package io.why503.accountservice.domain.auth.config;
 
 import io.why503.accountservice.domain.auth.service.impl.AuthenticationSuccessHandlerImpl;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -15,6 +19,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${custom.jwt.cookie-name}")
+    private String cookieName;
+
     @Bean
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http,
@@ -23,7 +31,19 @@ public class SecurityConfig {
                 .csrf((csrf -> csrf.disable()))     //csrf 설정 false
                 .cors(Customizer.withDefaults())    //다른 도메인에서 api호출 가능하게
                 .formLogin(form ->form  //post를 보낼 url
+                        .loginPage("/auth/login")    //로그인 페이지
+                        .loginProcessingUrl("/auth/login") //post보낼 위치
                         .successHandler(authenticationSuccessHandler)   //성공핸들러
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/auth/logout")
+                        .deleteCookies(cookieName)
+                        .logoutSuccessHandler(
+                                (request, response, authentication) -> {
+                                    expireCookie(response, cookieName);
+                                    response.setStatus(HttpServletResponse.SC_OK);
+                                }
+                        )
                 )
                 .sessionManagement((session) ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)  //서버에서 세션 안만듬, JWT로만 인증
@@ -33,5 +53,18 @@ public class SecurityConfig {
                                 .anyRequest().permitAll()
                 )
                 .build();
+    }
+    //쿠키 확실하게 삭제하는 함수
+    private void expireCookie(HttpServletResponse response, String findCookieName){
+        Cookie cookie = new Cookie(findCookieName, null);
+
+        //쿠키 속성
+        cookie.setMaxAge(0); // 쿠키 만료 시간
+        cookie.setHttpOnly(true); //  js는 접근 불가
+        //cookie.setSecure(true); // https에서만 사용
+        cookie.setSecure(false);
+        cookie.setPath("/"); //모든 경로 가능
+
+        response.addCookie(cookie);
     }
 }
--- FLOWIN/account-service/src/main/java/io/why503/accountservice/domain/auth/controller/LoginController.java
+++ FLOWIN/account-service/src/main/java/io/why503/accountservice/domain/auth/controller/LoginController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/login")
+@RequestMapping("/auth")
 public class LoginController {
     private final AccountService accountService;
 

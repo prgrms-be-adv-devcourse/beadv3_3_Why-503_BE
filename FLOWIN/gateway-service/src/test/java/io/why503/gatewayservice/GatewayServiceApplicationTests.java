1b534da480e170da004137dbee38e7aaa810a66b
hansj4873 <hansengjuu@naver.com>
2026-01-25 13:35:59 +0900

    [FI-777] test 추가

--- FLOWIN/gateway-service/src/test/java/io/why503/gatewayservice/GatewayServiceApplicationTests.java
+++ FLOWIN/gateway-service/src/test/java/io/why503/gatewayservice/GatewayServiceApplicationTests.java
@@ -1,11 +1,17 @@
 package io.why503.gatewayservice;
 
+import io.why503.gatewayservice.config.TestKeyConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.security.PublicKey;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@Import(TestKeyConfig.class)
 class GatewayServiceApplicationTests {
 
     @Test

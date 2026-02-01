e2bb19e73ed63ce669f7d066cab81c62c02512bd
S2Ruby <jwzang1004@gmail.com>
2026-02-01 13:42:06 +0900

    [FI-60] 스케줄러 문제 해결

---
+++ FLOWIN/payment-service/src/main/java/io/why503/paymentservice/global/config/SchedulingConfig.java
@@ -0,0 +1,10 @@
+package io.why503.paymentservice.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+    // 나중에 스케줄러 스레드 풀 설정 등을 여기서 관리할 수 있어 확장성도 좋습니다.
+}

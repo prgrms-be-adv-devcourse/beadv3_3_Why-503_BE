c67340255a9a961fdb8271714b75d96063881ff5
S2Ruby <jwzang1004@gmail.com>
2026-02-01 13:48:06 +0900

    [FI-60] 스케줄러 문제 해결

--- FLOWIN/payment-service/src/main/java/io/why503/paymentservice/global/config/SchedulingConfig.java
+++ FLOWIN/payment-service/src/main/java/io/why503/paymentservice/global/config/SchedulingConfig.java
@@ -1,10 +1,12 @@
 package io.why503.paymentservice.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
+@Profile("!test")
 public class SchedulingConfig {
     // 나중에 스케줄러 스레드 풀 설정 등을 여기서 관리할 수 있어 확장성도 좋습니다.
 }
--- FLOWIN/payment-service/src/test/java/io/why503/paymentservice/PaymentServiceApplicationTests.java
+++ FLOWIN/payment-service/src/test/java/io/why503/paymentservice/PaymentServiceApplicationTests.java
@@ -1,17 +1,15 @@
 package io.why503.paymentservice;
 
-import io.why503.paymentservice.global.config.SchedulingConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean; // 패키지 경로 주의!
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PaymentServiceApplicationTests {
-
-    @MockitoBean // @MockBean 대신 사용
-    private SchedulingConfig schedulingConfig;
 
     @Test
     void contextLoads() {
     }
+
 }

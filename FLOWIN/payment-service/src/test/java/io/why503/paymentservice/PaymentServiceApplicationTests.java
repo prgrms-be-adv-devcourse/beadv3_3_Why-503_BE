60e245e7dae762fdf3ca11a573e97d889f517916
S2Ruby <jwzang1004@gmail.com>
2026-02-01 13:45:43 +0900

    [FI-60] 스케줄러 문제 해결

--- FLOWIN/payment-service/src/test/java/io/why503/paymentservice/PaymentServiceApplicationTests.java
+++ FLOWIN/payment-service/src/test/java/io/why503/paymentservice/PaymentServiceApplicationTests.java
@@ -1,15 +1,17 @@
 package io.why503.paymentservice;
 
+import io.why503.paymentservice.global.config.SchedulingConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean; // 패키지 경로 주의!
 
 @SpringBootTest
-@ActiveProfiles("test")
 class PaymentServiceApplicationTests {
+
+    @MockitoBean // @MockBean 대신 사용
+    private SchedulingConfig schedulingConfig;
 
     @Test
     void contextLoads() {
     }
-
 }

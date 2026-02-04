a57db60a7c128ed9c7d4d2d8ad5fb1f82d2e2037
kdhaw <0517dhkim@gmail.com>
2026-02-04 10:36:41 +0900

    [FI-594] ai 학습 모델과 추천 시스템 추가

--- FLOWIN/ai-service/src/test/java/io/why503/aiservice/FakeAiTestConfig.java
+++ FLOWIN/ai-service/src/test/java/io/why503/aiservice/FakeAiTestConfig.java
@@ -2,14 +2,11 @@ package io.why503.aiservice;
 
 import org.mockito.Mockito;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
-import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest

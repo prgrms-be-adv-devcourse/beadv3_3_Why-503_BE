58549374ef422ce0736ed8fe0a1a9c74992d20b6
kimgiwoong127 <kimgiwoong127@gmail.com>
2026-02-06 09:15:58 +0900

    [FI-182] Entry, Queue 삭제

--- FLOWIN/gateway-service/src/main/java/io/why503/gatewayservice/entrytoken/exception/EntryTokenException.java
+++
@@ -1,8 +0,0 @@
-package io.why503.gatewayservice.entrytoken.exception;
-
-public abstract class EntryTokenException extends RuntimeException {
-
-    protected EntryTokenException(String message) {
-        super(message);
-    }
-}

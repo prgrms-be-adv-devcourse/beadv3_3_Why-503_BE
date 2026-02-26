a04d2ce88025785c28ed7cb55bb744525bdbb37f
hansj4873 <hansengjuu@naver.com>
2026-02-27 03:33:33 +0900

    [FI-1001] ai 수정

--- FLOWIN/ai-service/src/main/java/io/why503/aiservice/domain/ai/config/ChromaConfig.java
+++ FLOWIN/ai-service/src/main/java/io/why503/aiservice/domain/ai/config/ChromaConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
-@Configuration
+//@Configuration
 public class ChromaConfig {
 
     @Value("${spring.ai.vectorstore.chroma.client.host}")
